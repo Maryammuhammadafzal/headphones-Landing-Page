114a277f54218561e40a8cc3046607c81795341f
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-09 15:40:04 +0500

    creating new section

---
+++ src/app/highlights/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HighlightPage = () => {
+    return (
+        <div className='flex w-full justify-center items-center md:py-20 sm:py-14 xs:py-10 2xs:py-5 py-3 h-auto'>
+            <div className='lg:w-[90%] w-full h-auto flex justify-center items-center flex-col md:gap-10 sm:gap-6 gap-3'>
+                <h2 className='text-primary xl:text-8xl lg:text-[84px] md:text-[64px] sm:text-[56px] xs:text-[38px] 2xs:text-3xl max-2xs:leading-5 3xs:text-2xl text-xl uppercase text-center letter-spacing font-mono'>What Our Customers Say</h2>
+                <div className='w-full flex flex-wrap md:gap-6 sm:gap-4 gap-2 h-auto md:py-7 sm:py-5 py-3 justify-evenly items-center'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HighlightPage
