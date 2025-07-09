9a6d5d379346ec2122a28d7ea5a26dea4c804fe7
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-09 15:35:00 +0500

    creating work section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import TestimonialPage from "./testimonial/page";
+import WorkPage from "./work/page";
 
 export default function Home() {
   return (
 <div className="w-full h-auto overflow-hidden p-8 relative">
+  <WorkPage/>
   <TestimonialPage/>
 </div>
   );
