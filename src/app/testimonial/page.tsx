51b975a83866e7472208204a13d306ba390a1a46
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-09 14:46:23 +0500

    created testimonial section

--- src/app/testimonial/page.tsx
+++ src/app/testimonial/page.tsx
@@ -3,23 +3,40 @@ import Image from 'next/image'
 import React from 'react'
 
 const TestimonialPage = () => {
-    
+    const testimonial_data = [
+        {
+            name: 'Alex J.',
+            description: '"The sound quality is phenomenal, and the noise cancellation is a game-changer. I can’t imagine using anything else!"',
+        },
+        {
+            name: 'Alex J.',
+            description: '"The sound quality is phenomenal, and the noise cancellation is a game-changer. I can’t imagine using anything else!"',
+        },
+        {
+            name: 'Alex J.',
+            description: '"The sound quality is phenomenal, and the noise cancellation is a game-changer. I can’t imagine using anything else!"',
+        },
+    ]
     return (
-        <div className='flex w-full justify-center items-center py-20 h-auto'>
-            <div className='w-[90%] h-auto flex justify-center items-center flex-col gap-10'>
-                <h2 className='text-primary text-8xl uppercase letter-spacing font-mono'>What Our Customers Say</h2>
-                <div className='w-full flex flex-wrap h-auto py-7 justify-center items-center'>
-                    <Card className='shadow-none border-none rounded-none p-0 flex justify-center items-end w-[390px] h-[250px] relative'>
-                        <Image src='/images/testimonial-icon.png' alt='icon' width={64} height={64} className='absolute top-0 right-0' />
-                    <CardContent className='p-4 w-full h-auto bg-muted-foreground/20 rounded-2xl flex justify-center items-center flex-col gap-3'>
-                        <p className='text-xl font-sans text-primary text-center'>
-                            "The sound quality is phenomenal, and the noise cancellation is a game-changer. I can’t imagine using anything else!"
-                        </p>
-                        <h5 className='text-4xl font-mono text-primary capitalize'>
-                            Alex J.
-                        </h5>
-                    </CardContent>
-                    </Card>
+        <div className='flex w-full justify-center items-center md:py-20 sm:py-14 xs:py-10 2xs:py-5 py-3 h-auto'>
+            <div className='lg:w-[90%] w-full h-auto flex justify-center items-center flex-col md:gap-10 sm:gap-6 gap-3'>
+                <h2 className='text-primary xl:text-8xl lg:text-[84px] md:text-[64px] sm:text-[56px] xs:text-[38px] 2xs:text-3xl max-2xs:leading-5 3xs:text-2xl text-xl uppercase text-center letter-spacing font-mono'>What Our Customers Say</h2>
+                <div className='w-full flex flex-wrap md:gap-6 sm:gap-4 gap-2 h-auto md:py-7 sm:py-5 py-3 justify-evenly items-center'>
+                    {
+                        testimonial_data.map(({ description, name } , index) => (
+                            <Card key={index} className='shadow-none border-none rounded-none p-0 flex justify-center items-end max-3xs:w-full w-[390px] h-[250px] relative'>
+                                <Image src='/images/testimonial-icon.png' alt='icon' width={64} height={64} className='absolute top-0 right-0' />
+                                <CardContent className='xs:p-4 p-2 w-full h-auto bg-muted-foreground/20 rounded-2xl flex justify-center items-center flex-col gap-3'>
+                                    <p className='md:text-xl sm:text-lg 2xs:text-base 3xs:text-sm text-xs font-sans text-primary text-center'>
+                                        {`${description}`}
+                                    </p>
+                                    <h5 className=' md:text-4xl sm:text-3xl text-2xl font-mono text-primary capitalize'>
+                                        {name}
+                                    </h5>
+                                </CardContent>
+                            </Card>
+                        ))
+                    }
                 </div>
             </div>
         </div>
