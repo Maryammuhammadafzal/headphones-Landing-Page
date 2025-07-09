7a6098ddf3c18a2cbf06ef8f4ef2ecd10f37a529
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-09 14:15:26 +0500

    created 1 section

---
+++ src/app/testimonial/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TestimonialPage = () => {
+  return (
+    <div className='flex w-full justify-center items-center py-20 h-auto'>
+      <div className='w-[90%] h-auto flex justify-center items-center flex-col gap-10'>
+        <h2 className='text-primary text-8xl uppercase letter-spacing'>What Our Customers Say</h2>
+      </div>
+    </div>
+  )
+}
+
+export default TestimonialPage
