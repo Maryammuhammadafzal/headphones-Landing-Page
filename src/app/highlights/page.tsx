feaf5bb61cc4fac4739eb684ea9bc22759eebeac
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-09 15:59:52 +0500

    creating new section

--- src/app/highlights/page.tsx
+++ src/app/highlights/page.tsx
@@ -1,12 +1,61 @@
+import Image from 'next/image'
 import React from 'react'
 
 const HighlightPage = () => {
+    let highlight_data = [
+        {
+            title : 'High-Fidelity Audio',
+            image : '/images/highlight-icon1.png'
+        },
+        {
+            title : 'Customizable Sound Profiles',
+            image : '/images/highlight-icon2.png'
+        },
+        {
+            title : 'Built-In Voice Assistant',
+            image : '/images/highlight-icon3.png'
+        },
+        {
+            title : 'Foldable & Portable Design',
+            image : '/images/highlight-icon4.png'
+        },
+    ]
     return (
         <div className='flex w-full justify-center items-center md:py-20 sm:py-14 xs:py-10 2xs:py-5 py-3 h-auto'>
             <div className='lg:w-[90%] w-full h-auto flex justify-center items-center flex-col md:gap-10 sm:gap-6 gap-3'>
                 <h2 className='text-primary xl:text-8xl lg:text-[84px] md:text-[64px] sm:text-[56px] xs:text-[38px] 2xs:text-3xl max-2xs:leading-5 3xs:text-2xl text-xl uppercase text-center letter-spacing font-mono'>What Our Customers Say</h2>
                 <div className='w-full flex flex-wrap md:gap-6 sm:gap-4 gap-2 h-auto md:py-7 sm:py-5 py-3 justify-evenly items-center'>
-
+                  {
+                    highlight_data.map(({image , title}, index) => (
+                          <div key={index}
+                        className="flex relative justify-center items-center rounded-full bg-white h-[265px] w-[265px] "
+                    >
+                        <svg
+                            className="absolute inset-0 w-full h-full"
+                            preserveAspectRatio="none"
+                        >
+                            <rect
+                                x="0"
+                                y="0"
+                                width="100%"
+                                height="100%"
+                                rx="20%"
+                                ry="20%"
+                                fill="none"
+                                stroke="#000"
+                                strokeWidth="2"
+                                strokeDasharray="15 15"
+                            />
+                        </svg>
+                        <div className="relative w-full h-full flex justify-center items-center z-10 ">
+                            <div className=' flex justify-center items-center flex-col absolute gap-3 p-5 rounded-2xl'>
+                                <Image src={image} alt='icon' width={60} height={60} className='' />
+                                <h3 className='xs:text-[26px] 2xs:text-xl text-lg  font-bold font-sans text-primary text-center'>{title}</h3>
+                            </div>
+                        </div>
+                    </div>
+                    ))
+                  }
                 </div>
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import TestimonialPage from "./testimonial/page";
 import WorkPage from "./work/page";
+import HighlightPage from "./highlights/page";
 
 export default function Home() {
   return (
 <div className="w-full h-auto overflow-hidden p-8 relative">
+  <HighlightPage/>
   <WorkPage/>
   <TestimonialPage/>
 </div>
