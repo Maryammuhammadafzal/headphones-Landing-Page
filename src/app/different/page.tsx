4fa3fed156f08967aee27e090cc300eba979a9ce
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-10 11:11:00 +0500

    making responsive

--- src/app/different/page.tsx
+++ src/app/different/page.tsx
@@ -6,10 +6,10 @@ const DifferentPage = () => {
         <div className="flex w-full justify-center items-center md:py-20 sm:py-14 py-6 h-auto">
             <div className="xl:w-[90%] w-full flex flex-col items-center gap-10">
                 <h2 className="text-primary uppercase text-center font-mono leading-tight tracking-wide xl:text-8xl lg:text-[84px] md:text-[64px] sm:text-[48px] text-3xl">
-                    What Our Customers Say
+                    What Makes Us Different?
                 </h2>
 
-                <div className="w-full flex flex-col gap-6">
+                <div className="w-full flex flex-col gap-6 lg:py-10 md:py-7 sm:py-5 py-3">
 
                     <div className="grid md:grid-cols-5 grid-cols-1 gap-4 w-full">
                         {[1, 2].map((_, i) => (
@@ -45,22 +45,30 @@ const DifferentPage = () => {
                     </div>
 
                     <div className="w-full">
-                        <div className="relative bg-primary w-full h-[400px] rounded-[40px] overflow-hidden">
-                          
-                            <div className="absolute inset-0 flex flex-col justify-end items-start p-4 bg-gradient-to-t from-black/60 via-transparent">
-                                <Image
-                                    src="/images/work-icon.png"
-                                    alt="icon"
-                                    width={64}
-                                    height={64}
-                                />
-                                <div className="text-white mt-4">
-                                    <h3 className="text-2xl sm:text-3xl font-bold">
-                                        Built-in Microphones
+                        <div className="relative w-full h-[400px] rounded-[40px] overflow-hidden">
+
+                            <div className="absolute inset-0 flex flex-col justify-start items-center p-4 bg-gradient-to-t from-black/60 via-transparent">
+                                <h2 className='gradient-title xl:text-[231px] lg:text-[200px] lg:leading-[130px] md:text-[170px] md:leading-[110px] sm:text-[150px] sm:leading-[90px] xs:text-[124px] xs:leading-[70px] text-[100px] leading-[50px] min-[350px]:text-[80px] min-[350px]:leading-[64px] min-[280px]:text-[56px] min-[280px]:leading-[48px] font-mono uppercase xl:leading-[150px] letter-spacing'>Technology</h2>
+                                <div className="w-full h-full  flex justify-between items-center gap-4">
+                                    <h3 className="lg:text-5xl max-w-md text-primary-foreground text-4xl font-sans sm:text-3xl font-bold">
+                                        Customer-Centric Approach
                                     </h3>
-                                    <p className="text-sm sm:text-base mt-2 max-w-md">
-                                        Explore our collection and find the perfect pair of
-                                        headphones for your needs. Click below to shop now.
+
+                                    <Image
+                                        src='/images/boy-image.png'
+                                        alt="Customer"
+                                        layout="fill"
+                                        className="object-cover "
+                                    />
+                                    <Image
+                                        src='/images/boy-image1.png'
+                                        alt="Customer"
+                                        layout="fill"
+                                        className="object-cover absolute top-20 bottom-0 left-0 -z-10"
+                                    />
+
+                                    <p className="xl:text-xl text-lg max-w-sm px-3  mt-2 font-sans text-primary-foreground">
+                                        Your satisfaction is our priority. We offer a 30-day return policy and a dedicated support team to assist you.
                                     </p>
                                 </div>
                             </div>
