fd15c2953d67c02b3168c5e078a78ff845d3d29d
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-11 12:48:27 +0500

    Adding Mobile Menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
-import { MenuIcon } from 'lucide-react'
+'use client'
+import { MenuIcon, X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Header = () => {
+  const [IsMenuOpen, setIsMenuOpen] = useState(false);
+//   useEffect(() => {
+//   document.body.style.overflow = IsMenuOpen ? "hidden" : "auto";
+// }, [IsMenuOpen]);
   let nav_data = [
     {
       link: '/',
@@ -39,7 +44,35 @@ const Header = () => {
           }
         </ul>
         <div className='menu-icon w-auto h-auto sm:hidden flex'>
-          <MenuIcon size={40} color='#fff'/>
+          <MenuIcon size={40} color='#fff' onClick={() => setIsMenuOpen(true)} />
+        </div>
+        <div
+  className={`fixed top-0 right-0 z-50 h-screen w-[75%] sm:w-[50%] bg-white transition-transform duration-300 ease-in-out 
+    ${IsMenuOpen ? 'translate-x-0' : 'translate-x-full'} sm:hidden`}
+>
+
+          <ul className="w-full h-full flex gap-3 p-3 flex-col">
+            <li
+              className="menu-con w-full h-auto p-2 flex justify-end"
+              
+            >
+              <X onClick={() => setIsMenuOpen(false)} size={40}/>
+            </li>
+            {nav_data &&
+              nav_data.map((item, index) => (
+                <Link  key={index} href={item.link}>
+                  <li
+                   
+                    onClick={() => {
+                      setIsMenuOpen(false);
+                    }}
+                    className="px-3 py-3 border-b text-xl text-primary font-mono border-primary/20 text-left w-full h-auto"
+                  >
+                    {item.name}
+                  </li>
+                </Link>
+              ))}
+          </ul>
         </div>
       </nav>
 
